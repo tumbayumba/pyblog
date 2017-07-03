1133ce41f60d467b129ece6f534e9c9605e75a6f
AREON\RospopovS <Ser-19@ukr.net>
2017-07-03 18:17:16 +0300

    tutorial07

--- polls/models.py
+++ polls/models.py
@@ -2,7 +2,7 @@ import datetime
 from django.utils import timezone
 
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -12,7 +12,8 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
